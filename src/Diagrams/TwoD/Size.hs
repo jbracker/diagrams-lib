d7b6bdc36edd76e919d97c93e5c789840004338d
Jan Bracker <jan.bracker@googlemail.com>
2012-12-28 12:15:05 -0600

    Generalized booleans.

--- src/Diagrams/TwoD/Size.hs
+++ src/Diagrams/TwoD/Size.hs
@@ -28,6 +28,8 @@ module Diagrams.TwoD.Size
        , sized, sizedAs
        ) where
 
+import Prelude hiding (isInfinite)
+
 import Diagrams.Core
 import Diagrams.TwoD.Types
 import Diagrams.TwoD.Vector
@@ -38,42 +40,43 @@ import Control.Applicative ((<$>), liftA2)
 import Data.VectorSpace
 import Data.Basis
 import Data.MemoTrie
+import Data.Boolean
 
 ------------------------------------------------------------
 -- Computing diagram sizes
 ------------------------------------------------------------
 
 -- | Compute the width of an enveloped object.
-width :: (Enveloped a, V a ~ V2 b) => a -> b
+width :: (Num b, AdditiveGroup b, Enveloped a, V a ~ V2 b) => a -> Scalar (V2 b)
 width = maybe 0 (negate . uncurry (-)) . extentX
 
 -- | Compute the height of an enveloped object.
-height :: (Enveloped a, V a ~ V2 b) => a -> b
+height :: (Num b, AdditiveGroup b, Enveloped a, V a ~ V2 b) => a -> Scalar (V2 b)
 height = maybe 0 (negate . uncurry (-)) . extentY
 
 -- | Compute the width and height of an enveloped object.
-size2D :: (Enveloped a, V a ~ (V2 b)) => a -> (b, b)
+size2D :: (Num b, AdditiveGroup b, Enveloped a, V a ~ (V2 b)) => a -> (Scalar (V2 b), Scalar (V2 b))
 size2D = width &&& height
 
 -- | Compute the size of an enveloped object as a 'SizeSpec2D' value.
-sizeSpec2D :: (Enveloped a, V a ~ (V2 b)) => a -> SizeSpec2D b
+sizeSpec2D :: (Num b, AdditiveGroup b, Enveloped a, V a ~ (V2 b)) => a -> SizeSpec2D (Scalar (V2 b))
 sizeSpec2D = uncurry Dims . size2D
 
 -- | Compute the absolute  x-coordinate range of an enveloped object in
 --   R2, in  the form (lo,hi).   Return @Nothing@ for objects  with an
 --   empty envelope.
-extentX :: (Enveloped a, V a ~ V2 b) => a -> Maybe (b, b)
+extentX :: (Num b, AdditiveGroup b, Enveloped a, V a ~ V2 b) => a -> Maybe (Scalar (V2 b), Scalar (V2 b))
 extentX d = (\f -> (-f unit_X, f unitX)) <$> (appEnvelope . getEnvelope $ d)
 
 -- | Compute the absolute y-coordinate range of an enveloped object in
 --   R2, in the form (lo,hi).
-extentY :: (Enveloped a, V a ~ V2 b) => a -> Maybe (b, b)
+extentY :: (Num b, AdditiveGroup b, Enveloped a, V a ~ V2 b) => a -> Maybe (Scalar (V2 b), Scalar (V2 b))
 extentY d = (\f -> (-f unit_Y, f unitY)) <$> (appEnvelope . getEnvelope $ d)
 
 -- | Compute the point at the center (in the x- and y-directions) of a
 --   enveloped object.  Return the origin for objects with an empty
 --   envelope.
-center2D :: (Enveloped a, V a ~ V2 b) => a -> P2 b
+center2D :: (Num b, AdditiveGroup b, Enveloped a, V a ~ V2 b) => a -> P2 (Scalar (V2 b))
 center2D = maybe origin (p2 . (mid *** mid)) . mm . (extentX &&& extentY)
   where mm = uncurry (liftA2 (,))
         mid = (/2) . uncurry (+)
@@ -108,8 +111,9 @@ mkSizeSpec (Just w) (Just h) = Dims w h
 -- | @requiredScaleT spec sz@ returns a transformation (a uniform scale)
 --   which can be applied to something of @size@ to make it fit the
 --   requested size @spec@, without changing the aspect ratio.
-requiredScaleT :: ( Ord a
-                  , RealFloat a
+requiredScaleT :: ( EqB a
+                  , RealFloatB a
+                  , IfB a
                   , HasBasis a
                   , HasTrie (Basis a)
                   , Scalar a ~ a
@@ -122,28 +126,28 @@ requiredScaleT spec size = scaling (requiredScale spec size)
 --   specification of both dimensions may not be honored if the aspect
 --   ratios do not match; in that case the scaling will be as large as
 --   possible so that the object still fits within the requested size.
-requiredScale :: ( Ord a
-                 , RealFloat a
+requiredScale :: ( EqB a
+                 , RealFloatB a
+                 , IfB a
                  ) => SizeSpec2D a -> (a, a) -> a
 requiredScale Absolute _    = 1
-requiredScale (Width wSpec) (w,_)
-  | wSpec == 0 || w == 0 = 1
-  | otherwise            = wSpec / w
-requiredScale (Height hSpec) (_,h)
-  | hSpec == 0 || h == 0 = 1
-  | otherwise            = hSpec / h
+requiredScale (Width wSpec) (w,_) = 
+  ifB (wSpec ==* 0 ||* w ==* 0) (1) (wSpec / w)
+requiredScale (Height hSpec) (_,h) =
+  ifB (hSpec ==* 0 ||* h ==* 0) (1) (hSpec / h)
 requiredScale (Dims wSpec hSpec) (w,h) = s
   where xscale  = wSpec / w
         yscale  = hSpec / h
         s'      = min xscale yscale
-        s | isInfinite s' = 1
-          | otherwise     = s'
+        s = ifB (isInfinite s') (1) (s')
 
 -- | Uniformly scale any enveloped object so that it fits within the
 --   given size.
 sized :: ( Transformable a
          , Enveloped a
-         , RealFloat b
+         , RealFloatB b
+         , IfB b
+         , EqB b
          , HasBasis b
          , b ~ Scalar b
          , HasTrie (Basis b)
@@ -155,7 +159,9 @@ sized spec a = transform (requiredScaleT spec (size2D a)) a
 --   size as\" (fits within the width and height of) some other
 --   object.
 sizedAs :: ( Transformable a
-           , RealFloat c
+           , RealFloatB c
+           , IfB c
+           , EqB c
            , HasBasis c
            , HasTrie (Basis c)
            , c ~ Scalar c
